07a439f1adfc25740cf3f4639435aee022d6985d
FlaviaPardo <flavia.pardval@gmail.com>
2023-05-18 16:45:52 -0500

    Funciones reajuste
    - imagen en una sola medida
    - calidad más alta
    - contraste alto

--- FuncionReajuste.py
+++ FuncionReajuste.py
@@ -1,16 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 import shutil
-# Ruta de la imagen que deseas cargar
-#ruta_imagen = "C:/Users/FLAVIA/Downloads/Python/prueba2.jpg"
-
-# Abrir la imagen
-#imagen = Image.open(ruta_imagen)
-#imagen.show()
-# Mostrar información de la imagen
-#print("Formato de la imagen:", imagen.format)
-#print("Dimensiones de la imagen:", imagen.size)
-#print("Modo de la imagen:", imagen.mode)
 
 ruta_carpeta = 'C:/Users/FLAVIA/Downloads/Python/misimagenes'
 ruta_destino = 'C:/Users/FLAVIA/Downloads/redimensionado'
@@ -38,27 +28,32 @@ for nombre_archivo in nuevos_archivos:
     ancho, alto = imagen.size
     
     if extension in ['.jpg', '.jpeg', '.png', '.gif']:
-        if ancho < 1000 and alto < 1000:
-            nuevo_ancho = ancho * 2
-            nuevo_alto = alto * 2
-            nombre_procesado = archivos_procesados[-1]
-            ruta_destino_archivo = os.path.join(ruta_destino, nombre_procesado)
-            
-            imagen_redimensionada = imagen.resize((nuevo_ancho, nuevo_alto), resample=Image.LANCZOS)
-            imagen_redimensionada.save(ruta_destino_archivo)
-            imagen_redimensionada.show()
-            os.startfile(ruta_destino_archivo)
-
-            # Imprime los tamaños originales y redimensionados
-            print(f"Tamaño original: {ancho}x{alto}")
-            print(f"Tamaño redimensionado: {nuevo_ancho}x{nuevo_alto}")
+        nuevo_ancho = 1920
+        nuevo_alto = 1080
+        
+        # Ajustar las nuevas dimensiones para mantener la relación de aspecto
+        relacion_aspecto = ancho / alto
+        if relacion_aspecto > 1:
+            nuevo_alto = int(nuevo_ancho / relacion_aspecto)
         else:
-            nombre_procesado = archivos_procesados[-1]
-            ruta_destino_archivo = os.path.join(ruta_destino, nombre_procesado)
-            
-            shutil.copyfile(ruta_original, ruta_destino_archivo)
-            imagen.show()
-            os.startfile(ruta_original)
-            print(f"Tamaño de la imagen: {ancho}x{alto}")
-    else:
-        print(f"{nombre_archivo} no es una imagen")
+            nuevo_ancho = int(nuevo_alto * relacion_aspecto)
+        
+        # Redimensionar la imagen original a las nuevas dimensiones
+        imagen_redimensionada = imagen.resize((nuevo_ancho, nuevo_alto), resample=Image.LANCZOS)
+        
+        # Aumentar el contraste de la imagen
+        factor_contraste = 1.5  # Ajusta este valor para aumentar o disminuir el contraste
+        realce_contraste = ImageEnhance.Contrast(imagen_redimensionada)
+        imagen_contraste = realce_contraste.enhance(factor_contraste)
+        
+        nombre_procesado = archivos_procesados[-1]
+        ruta_destino_archivo = os.path.join(ruta_destino, nombre_procesado)
+        
+        # Guardar la imagen redimensionada y con mayor contraste en formato JPEG
+        imagen_contraste.save(ruta_destino_archivo, format='JPEG', quality=95)
+        imagen_contraste.show()
+        os.startfile(ruta_destino_archivo)
+        
+        # Imprime los tamaños originales y redimensionados
+        print(f"Tamaño original: {ancho}x{alto}")
+        print(f"Tamaño redimensionado: {nuevo_ancho}")
